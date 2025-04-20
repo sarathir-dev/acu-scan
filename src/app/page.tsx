c72e794bd7c559df0c97b1064924395707ea84ee
sarathir-dev <sarathir.dev@gmail.com>
2025-04-20 20:24:26 +0530

    Updated hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+// import Image from "next/image";
+// import { AspectRatio } from "@/components/ui/aspect-ratio";
+// import MedicalImaging from "../../public/assets/images/MedicalImaging.png";
+// import { motion } from "motion/react";
+import { HeroSectionOne } from "@/components/hero-section";
+
 export default function Home() {
   return (
-    <div className="">
-      <h1>From Home Page</h1>
+    <div>
+      <HeroSectionOne />
     </div>
   );
 }
